8351df6eecd5cd00c6cd0d17a1d037dbbf97d69e
henrychrisostomo1 <HENRYCHRISOSTOMO1@GMAIL.COM>
2025-08-15 01:54:44 +0000

    Entrada de dados do usuário

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
 
-int main() {
-    int pontos = 50;
-    int habitantes = 12325000;
-    float area =  1521.11;
-    double pib = 699.28;
-    char inicial = 'A';
-    char cidade[20] = "São Paulo";
+int main(){
+    int pontos;
+    int habitantes;   
+    float area;
+    float pib; 
+    char inicial;
+    char codigo;
+    char cidade[20];
 
-    printf("pontos: %d pontos turisticos \n", pontos);
-    printf("habitantes: %d população \n", habitantes);
-    printf("area: %.2f km \n", area);
-    printf("pib: %.2f reais \n", pib);
-    printf("Inicial do Estado: %c \n", inicial);
-    printf("Nome da Cidade: %s \n", cidade);
+    printf("Digite os pontos turisticos: \n");
+    scanf("%d", &pontos);
+
+    printf("Digite os habitantes: \n");
+    scanf("%d", &habitantes);
+
+    printf("Digite a área: \n");
+    scanf("%f", &area); 
+
+    printf("Digite o Pib: \n");
+    scanf("%f", &pib);
+
+    printf("Digite a Inicial: \n");
+    scanf("%s", &inicial);
+
+    printf("Digite o Código: \n");
+    scanf("%s", &codigo);
+
+    printf("Digite a Cidade:\n");
+    scanf("%s", cidade);
+
+    printf("nome da cidade: %s - inicial: %s - código da carta: %s", cidade, inicial, codigo);
+    printf("pontos turísticos: %d - população: %d - km2: %f - Pib: %f", pontos, habitantes, area, pib);
 
 
-    
     return 0;
 
-}
 
+
+
+     
+
+}
